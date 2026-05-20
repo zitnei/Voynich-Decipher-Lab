af4188691e3c1c03712a8c1fc24d202679c566a2
zintei <konasato0709@gmail.com>
2026-05-20 13:28:02 +0900

    Fix filenames

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' }
-    ]
+    unoptimized: true
   }
 };
+
 export default nextConfig;
